c842f5c85ce171adc7ec7e2e805bb841ea18ce9e
George Ang <gnap.an@gmail.com>
2022-02-25 18:16:10 +0800

    fb.

--- examples/pytorch/benchmarking/deepspeed_benchmark.py
+++ examples/pytorch/benchmarking/deepspeed_benchmark.py
@@ -42,7 +42,7 @@ class CustomBenchmarkArguments(PyTorchBenchmarkArguments):
     def __init__(self, **kwargs):
         self.local_rank = kwargs.pop("local_rank", self.local_rank)
         self.deepspeed_config = kwargs.pop("deepspeed_config", self.deepspeed_config)
-        super().__init__(**kwargs)
+        super(CustomBenchmarkArguments).__init__(**kwargs)
 
     deepspeed_config: str = field(default='tests/deepspeed/ds_config_zero3.json', metadata={"help": "deepspeed_config file"})
     local_rank: int = field(default=0, metadata={"help": "local rank of the worker process"})
